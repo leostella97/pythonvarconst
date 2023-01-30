f4a14b9b02858fa0ef66a23e9c43c40e40764eee
Leonardo Stella de Oliveira <Leonstella97@gmail.com>
2023-01-30 09:04:02 -0300

    Adição dos arquivos

---
+++ variaveisConstantes.py
@@ -0,0 +1,14 @@
+nome = "Leonardo"
+idade = 26
+
+nome, idade = "Daniela", 29
+
+print(nome, idade)
+
+#para separar nome da variável usa padrão snake case, separados por underline
+limite_saque_diario = 1000
+print(f"O limite de saque diário é {limite_saque_diario}")
+
+#constantes
+BRAZILIAN_STATES = ["SP", "RJ", "SC", "RS"]
+print(BRAZILIAN_STATES)
---
+++ variaveis_e_constantes.py
@@ -0,0 +1,9 @@
+#maiúsculo indica que é constante para não ser alterado
+ABS_PATH = 'D:\Python\Variáveis e Constantes'
+DEBUG = True
+STATES = [
+    'SP',
+    'RJ',
+    'MG',
+]
+AMOUNT = 30.2
